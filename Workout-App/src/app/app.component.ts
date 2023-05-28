8a678da0c08a9163341cc13fbddfa076de97b0be
vergaGabriel <gabriel_verga@hotmail.com>
2023-05-28 20:27:11 -0300

    modified:   Workout-App/src/app/app.component.html 	modified:   Workout-App/src/app/app.component.spec.ts 	modified:   Workout-App/src/app/app.component.ts

--- Workout-App/src/app/app.component.ts
+++ Workout-App/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActionSheetController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,9 @@ import { ActionSheetController } from '@ionic/angular';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private actionSheetCtrl: ActionSheetController) {}
-  
+  constructor(private actionSheetCtrl: ActionSheetController,
+              private alertController: AlertController) {} 
+
   async presentActionSheet() {
     const actionSheet =  await this.actionSheetCtrl.create({
       header: 'Actions', buttons: [
@@ -37,4 +39,16 @@ export class AppComponent {
     
     await actionSheet.present()
   }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'This is an alert!',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
 }
